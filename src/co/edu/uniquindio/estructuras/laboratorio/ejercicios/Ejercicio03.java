fa0c1c2af61e3e07083119de4e5800b6aad99dd4
Breyner-sq <93687667+Breyner-sq@users.noreply.github.com>
2024-03-08 08:03:44 -0500

    ejercicio 03
    en proceso

---
+++ src/co/edu/uniquindio/estructuras/laboratorio/ejercicios/Ejercicio03.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.estructuras.laboratorio.ejercicios;
+import co.edu.uniquindio.estructuras.laboratorio.listas.SimpleList;
+
+public class Ejercicio03 {
+	
+	public static void eliminarNumerosPares(SimpleList<Integer> lista) {
+			SimpleList<Integer> listaNumeros = new SimpleList<Integer>();
+			Iterator<Integer> iterador = listaNumeros.iterator(); 
+			
+		
+	}
+
+}
