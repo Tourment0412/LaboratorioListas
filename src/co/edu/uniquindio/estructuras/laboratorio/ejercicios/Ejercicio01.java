0ef543bb2eb88fb9ea51876173ca3d0a0129adc0
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-03-08 08:05:32 -0500

    Ejercicio01 done

---
+++ src/co/edu/uniquindio/estructuras/laboratorio/ejercicios/Ejercicio01.java
@@ -0,0 +1,40 @@
+package co.edu.uniquindio.estructuras.laboratorio.ejercicios;
+
+import co.edu.uniquindio.estructuras.laboratorio.listas.SimpleList;
+import co.edu.uniquindio.estructuras.laboratorio.listas.SimpleNode;
+
+public class Ejercicio01 {
+
+	public static void main(String []args ) {
+		SimpleList<Integer> lista= new SimpleList<Integer>();
+		lista.addToTail(1);
+		lista.addToTail(2);
+		lista.addToTail(3);
+		SimpleList <Integer> listaResult= obtenerNumerosPosImpares(lista);
+		imprimirLista(listaResult);
+	}
+
+	public static SimpleList<Integer> obtenerNumerosPosImpares(SimpleList<Integer> lista) {
+		SimpleList<Integer> listaRetorno = new SimpleList<Integer>();
+		SimpleNode<Integer> referencia = lista.head;
+		int cont = 0;
+		while (referencia!= null) {
+			if (cont % 2 != 0) {
+				listaRetorno.addToTail(referencia.getValue());
+
+			}
+			referencia = referencia.getNext();
+			cont++;
+		}
+		return listaRetorno;
+	}
+	
+	public static void imprimirLista(SimpleList<Integer>lista) {
+		SimpleNode<Integer> nodoRef= lista.head;
+		while(nodoRef!=null) {
+			System.out.println(nodoRef.getValue());
+			nodoRef= nodoRef.getNext();
+		}
+	}
+
+}
